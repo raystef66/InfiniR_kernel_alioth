5e51b794d916639f4d51a0d419d5702761d656fa
Neal Cardwell <ncardwell@google.com>
2017-10-12 23:44:27 -0400

    FROMGIT: net-tcp_bbr: v2: count packets lost over TCP rate sampling interval
    For understanding the relationship between inflight and packet loss
    signals, to try to find the highest inflight value that has acceptable
    levels of packet losses.
    
    Effort: net-tcp_bbr
    Origin-9xx-SHA1: 4527e26b2bd7756a88b5b9ef1ada3da33dd609ab
    Change-Id: I594c2500868d9c530770e7ddd68ffc87c57f4fd5
    (cherry-picked from https://github.com/google/bbr/commit/ebe09b73d0a591b1bff4aa4f8c95878b4711ca4d)
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    (cherry picked from commit 09331181cb19e5071015aed3f36250ca1f6ee5d3)
    (cherry picked from commit 4888fc7f804fb24448a4b970a26dc2051cd88a52)
    (cherry picked from commit 5c7b25290305d10b78567adc783731d962dabc24)
    (cherry picked from commit b6b09bb89d9083ae0ee727ea4bb3e03ce0063ab9)
    (cherry picked from commit 58f39b69500f0e3239018ad96220b91754fc6602)
    (cherry picked from commit 6e2d98c185ed952058014eb352e1422fd7cd3c4c)
    (cherry picked from commit fa145309ba99414246a509048ce45340a91ea55a)
    (cherry picked from commit 7b4cbbd4d594c09fe1086e9efcf2f245135bf22f)
    (cherry picked from commit 8271f91681b970fcb9839ad480741fcd07fb6855)
    (cherry picked from commit 9de84682e6fa3b82a455618cf9b102ad604c9947)
    (cherry picked from commit 59c26fbde506eee1cfb20162fd15a88978f68e02)
    (cherry picked from commit 0c3e47a987e787e61fea0d9b352a6ea237439a06)

--- net/ipv4/tcp_rate.c
+++ net/ipv4/tcp_rate.c
@@ -63,6 +63,8 @@ void tcp_rate_skb_sent(struct sock *sk, struct sk_buff *skb)
 	TCP_SKB_CB(skb)->tx.first_tx_mstamp	= tp->first_tx_mstamp;
 	TCP_SKB_CB(skb)->tx.delivered_mstamp	= tp->delivered_mstamp;
 	TCP_SKB_CB(skb)->tx.delivered		= tp->delivered;
+	TCP_SKB_CB(skb)->tx.delivered_ce	= tp->delivered_ce;
+	TCP_SKB_CB(skb)->tx.lost		= tp->lost;
 	TCP_SKB_CB(skb)->tx.is_app_limited	= tp->app_limited ? 1 : 0;
 }
 
@@ -84,6 +86,8 @@ void tcp_rate_skb_delivered(struct sock *sk, struct sk_buff *skb,
 
 	if (!rs->prior_delivered ||
 	    after(scb->tx.delivered, rs->prior_delivered)) {
+		rs->prior_lost	     = scb->tx.lost;
+		rs->prior_delivered_ce  = scb->tx.delivered_ce;
 		rs->prior_delivered  = scb->tx.delivered;
 		rs->prior_mstamp     = scb->tx.delivered_mstamp;
 		rs->is_app_limited   = scb->tx.is_app_limited;
@@ -135,7 +139,12 @@ void tcp_rate_gen(struct sock *sk, u32 delivered, u32 lost,
 		rs->interval_us = -1;
 		return;
 	}
-	rs->delivered_ce   = tp->delivered - rs->prior_delivered;
+	rs->delivered   = tp->delivered - rs->prior_delivered;
+	rs->lost        = tp->lost - rs->prior_lost;
+
+	rs->delivered_ce = tp->delivered_ce - rs->prior_delivered_ce;
+	/* delivered_ce occupies less than 32 bits in the skb control block */
+	rs->delivered_ce &= TCPCB_DELIVERED_CE_MASK;
 
 	/* Model sending data and receiving ACKs as separate pipeline phases
 	 * for a window. Usually the ACK phase is longer, but with ACK
