ea51cded9cd31f116ad812a1c4f00107a9a62ec7
Francisco Franco <franciscofranco.1990@gmail.com>
2018-06-14 22:01:24 +0000

    msm_performance: Kill userspace boosting
    Change-Id: I437ca294d96522152e328f4e4dd550f0ad27fc00
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -83,6 +83,7 @@ device_param_cb(touchboost, &param_ops_touchboost, NULL, 0644);
 
 static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 {
+#if 0
 	int i, j, ntokens = 0;
 	unsigned int val, cpu;
 	const char *cp = buf;
@@ -147,6 +148,7 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 			cpumask_clear_cpu(j, limit_mask);
 	}
 	put_online_cpus();
+#endif
 
 	return 0;
 }
@@ -172,6 +174,7 @@ module_param_cb(cpu_min_freq, &param_ops_cpu_min_freq, NULL, 0644);
 
 static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 {
+#if 0
 	int i, j, ntokens = 0;
 	unsigned int val, cpu;
 	const char *cp = buf;
@@ -223,6 +226,7 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 			cpumask_clear_cpu(j, limit_mask);
 	}
 	put_online_cpus();
+#endif
 
 	return 0;
 }
