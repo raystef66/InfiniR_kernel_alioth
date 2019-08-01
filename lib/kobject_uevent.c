359ed11dd5b8823dec80de86d631b0d5b0f38800
Sultan Alsawaf <sultan@kerneltoast.com>
2019-07-31 18:27:02 -0700

    kobject_uevent: Allocate environment buffer on the stack
    The environment buffer isn't very big; when it's allocated on the stack,
    kobject_uevent_env's stack frame size increases to just over 2 KiB,
    which is safe considering that we have a 16 KiB stack.
    
    Allocate the environment buffer on the stack instead of using the slab
    allocator in order to improve performance.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    [ Tashar02: Adapt to k4.19 ]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- lib/kobject_uevent.c
+++ lib/kobject_uevent.c
@@ -566,7 +566,7 @@ static void zap_modalias_env(struct kobj_uevent_env *env)
 int kobject_uevent_env(struct kobject *kobj, enum kobject_action action,
 		       char *envp_ext[])
 {
-	struct kobj_uevent_env *env;
+	struct kobj_uevent_env env;
 	const char *action_string = kobject_actions[action];
 	const char *devpath = NULL;
 	const char *subsystem;
@@ -629,11 +629,6 @@ int kobject_uevent_env(struct kobject *kobj, enum kobject_action action,
 		return 0;
 	}
 
-	/* environment buffer */
-	env = kzalloc(sizeof(struct kobj_uevent_env), GFP_KERNEL);
-	if (!env)
-		return -ENOMEM;
-
 	/* complete object path */
 	devpath = kobject_get_path(kobj, GFP_KERNEL);
 	if (!devpath) {
@@ -641,21 +636,23 @@ int kobject_uevent_env(struct kobject *kobj, enum kobject_action action,
 		goto exit;
 	}
 
+	memset(&env, 0, sizeof(env));
+
 	/* default keys */
-	retval = add_uevent_var(env, "ACTION=%s", action_string);
+	retval = add_uevent_var(&env, "ACTION=%s", action_string);
 	if (retval)
 		goto exit;
-	retval = add_uevent_var(env, "DEVPATH=%s", devpath);
+	retval = add_uevent_var(&env, "DEVPATH=%s", devpath);
 	if (retval)
 		goto exit;
-	retval = add_uevent_var(env, "SUBSYSTEM=%s", subsystem);
+	retval = add_uevent_var(&env, "SUBSYSTEM=%s", subsystem);
 	if (retval)
 		goto exit;
 
 	/* keys passed in from the caller */
 	if (envp_ext) {
 		for (i = 0; envp_ext[i]; i++) {
-			retval = add_uevent_var(env, "%s", envp_ext[i]);
+			retval = add_uevent_var(&env, "%s", envp_ext[i]);
 			if (retval)
 				goto exit;
 		}
@@ -663,7 +660,7 @@ int kobject_uevent_env(struct kobject *kobj, enum kobject_action action,
 
 	/* let the kset specific function add its stuff */
 	if (uevent_ops && uevent_ops->uevent) {
-		retval = uevent_ops->uevent(kset, kobj, env);
+		retval = uevent_ops->uevent(kset, kobj, &env);
 		if (retval) {
 			pr_debug("kobject: '%s' (%p): %s: uevent() returned "
 				 "%d\n", kobject_name(kobj), kobj,
@@ -685,7 +682,7 @@ int kobject_uevent_env(struct kobject *kobj, enum kobject_action action,
 		break;
 
 	case KOBJ_UNBIND:
-		zap_modalias_env(env);
+		zap_modalias_env(&env);
 		break;
 
 	default:
@@ -694,12 +691,12 @@ int kobject_uevent_env(struct kobject *kobj, enum kobject_action action,
 
 	mutex_lock(&uevent_sock_mutex);
 	/* we will send an event, so request a new sequence number */
-	retval = add_uevent_var(env, "SEQNUM=%llu", (unsigned long long)++uevent_seqnum);
+	retval = add_uevent_var(&env, "SEQNUM=%llu", (unsigned long long)++uevent_seqnum);
 	if (retval) {
 		mutex_unlock(&uevent_sock_mutex);
 		goto exit;
 	}
-	retval = kobject_uevent_net_broadcast(kobj, env, action_string,
+	retval = kobject_uevent_net_broadcast(kobj, &env, action_string,
 					      devpath);
 	mutex_unlock(&uevent_sock_mutex);
 
@@ -708,25 +705,23 @@ int kobject_uevent_env(struct kobject *kobj, enum kobject_action action,
 	if (uevent_helper[0] && !kobj_usermode_filter(kobj)) {
 		struct subprocess_info *info;
 
-		retval = add_uevent_var(env, "HOME=/");
+		retval = add_uevent_var(&env, "HOME=/");
 		if (retval)
 			goto exit;
-		retval = add_uevent_var(env,
+		retval = add_uevent_var(&env,
 					"PATH=/sbin:/bin:/usr/sbin:/usr/bin");
 		if (retval)
 			goto exit;
-		retval = init_uevent_argv(env, subsystem);
+		retval = init_uevent_argv(&env, subsystem);
 		if (retval)
 			goto exit;
 
 		retval = -ENOMEM;
-		info = call_usermodehelper_setup(env->argv[0], env->argv,
-						 env->envp, GFP_KERNEL,
-						 NULL, cleanup_uevent_env, env);
-		if (info) {
+		info = call_usermodehelper_setup(env.argv[0], env.argv,
+						 env.envp, GFP_KERNEL,
+						 NULL, cleanup_uevent_env, &env);
+		if (info)
 			retval = call_usermodehelper_exec(info, UMH_NO_WAIT);
-			env = NULL;	/* freed by cleanup_uevent_env */
-		}
 	}
 #endif
 
@@ -736,7 +731,6 @@ int kobject_uevent_env(struct kobject *kobj, enum kobject_action action,
 
 exit:
 	kfree(devpath);
-	kfree(env);
 	return retval;
 }
 EXPORT_SYMBOL_GPL(kobject_uevent_env);
