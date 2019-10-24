d9351e1a54beae7ca505c0b377379ec59686897f
Jordan Crouse <jcrouse@codeaurora.org>
2019-10-24 09:55:14 -0600

    msm: kgsl: Use DMA APIs for memory pool cache maintenance
    After allocating and zeroing pages from system memory or the pool we need
    to ensure that the cache is synchronized so that it doesn't cause problems
    down the road.  Use dma_sync_sg_for_device to make sure the allocated pages
    are clean. This isn't the best way to handle this but we haven't yet come
    up with a better way and this does the job.
    
    Change-Id: Ic0dedbade48b700015bec172cf9b64e436364b4a
    Signed-off-by: Jordan Crouse <jcrouse@codeaurora.org>
    [Reina: Adapt for 4.19]
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/gpu/msm/kgsl_pool.c
+++ drivers/gpu/msm/kgsl_pool.c
@@ -71,8 +71,6 @@ _kgsl_pool_add_page(struct kgsl_page_pool *pool, struct page *p)
 		return;
 	}
 
-	kgsl_zero_page(p, pool->pool_order);
-
 	llist_add((struct llist_node *)&p->lru, &pool->page_list);
 	atomic_inc(&pool->page_count);
 	mod_node_page_state(page_pgdat(p), NR_KERNEL_MISC_RECLAIMABLE,
@@ -274,7 +272,8 @@ static int kgsl_pool_get_retry_order(unsigned int order)
  * Return total page count on success and negative value on failure
  */
 int kgsl_pool_alloc_page(int *page_size, struct page **pages,
-			unsigned int pages_len, unsigned int *align)
+			unsigned int pages_len, unsigned int *align,
+			struct device *dev)
 {
 	int j;
 	int pcount = 0;
@@ -300,7 +299,6 @@ int kgsl_pool_alloc_page(int *page_size, struct page **pages,
 			} else
 				return -ENOMEM;
 		}
-		kgsl_zero_page(page, order);
 		goto done;
 	}
 
@@ -318,7 +316,6 @@ int kgsl_pool_alloc_page(int *page_size, struct page **pages,
 			page = _kgsl_alloc_pages(order);
 			if (page == NULL)
 				return -ENOMEM;
-			kgsl_zero_page(page, order);
 			goto done;
 		}
 	}
@@ -346,10 +343,10 @@ int kgsl_pool_alloc_page(int *page_size, struct page **pages,
 				return -ENOMEM;
 		}
 
-		kgsl_zero_page(page, order);
 	}
 
 done:
+	kgsl_zero_page(page, order, dev);
 	for (j = 0; j < (*page_size >> PAGE_SHIFT); j++) {
 		p = nth_page(page, j);
 		pages[pcount] = p;
