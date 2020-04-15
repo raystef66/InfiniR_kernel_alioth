783057339ae9d2ca6d60caf1014099cbbea235a2
Valentin Schneider <valentin.schneider@arm.com>
2020-04-15 22:05:06 +0100

    UPSTREAM: sched: Remove checks against SD_LOAD_BALANCE
    The SD_LOAD_BALANCE flag is set unconditionally for all domains in
    sd_init(). By making the sched_domain->flags syctl interface read-only, we
    have removed the last piece of code that could clear that flag - as such,
    it will now be always present. Rather than to keep carrying it along, we
    can work towards getting rid of it entirely.
    
    cpusets don't need it because they can make CPUs be attached to the NULL
    domain (e.g. cpuset with sched_load_balance=0), or to a partitioned
    root_domain, i.e. a sched_domain hierarchy that doesn't span the entire
    system (e.g. root cpuset with sched_load_balance=0 and sibling cpusets with
    sched_load_balance=1).
    
    isolcpus apply the same "trick": isolated CPUs are explicitly taken out of
    the sched_domain rebuild (using housekeeping_cpumask()), so they get the
    NULL domain treatment as well.
    
    Remove the checks against SD_LOAD_BALANCE.
    
    Signed-off-by: Valentin Schneider <valentin.schneider@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20200415210512.805-4-valentin.schneider@arm.com
    (cherry picked from commit e669ac8ab952df2f07dee1e1efbf40647d6de332)
    
    BUG=b:168337303
    TEST=CQ compiles; future patches easier to pick
    
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Change-Id: I3a9463cf818a5d38bd2f131cda2a218bca445809
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3339375
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Joel Fernandes <joelaf@google.com>
    Commit-Queue: Joel Fernandes <joelaf@google.com>
    Tested-by: Joel Fernandes <joelaf@google.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8234,6 +8234,7 @@ select_task_rq_fair(struct task_struct *p, int prev_cpu, int sd_flag, int wake_f
 sd_loop:
 	rcu_read_lock();
 	for_each_domain(cpu, tmp) {
+
 		if (!(tmp->flags & SD_LOAD_BALANCE))
 			break;
 
@@ -11462,9 +11463,8 @@ static int active_load_balance_cpu_stop(void *data)
 	/* Search for an sd spanning us and the target CPU. */
 	rcu_read_lock();
 	for_each_domain(target_cpu, sd) {
-		if ((sd->flags & SD_LOAD_BALANCE) &&
-		    cpumask_test_cpu(busiest_cpu, sched_domain_span(sd)))
-				break;
+		if (cpumask_test_cpu(busiest_cpu, sched_domain_span(sd)))
+			break;
 	}
 
 	if (likely(sd)) {
@@ -11594,10 +11594,6 @@ static void rebalance_domains(struct rq *rq, enum cpu_idle_type idle)
 		if (!sd_overutilized(sd) && !prefer_spread_on_idle(cpu,
 					idle == CPU_NEWLY_IDLE))
 			continue;
-
-		if (!(sd->flags & SD_LOAD_BALANCE))
-			continue;
-
 		/*
 		 * Stop the load balance at this level. There is another
 		 * CPU in our sched group which is doing load balancing more
@@ -12265,6 +12261,7 @@ int newidle_balance(struct rq *this_rq, struct rq_flags *rf)
 	for_each_domain(this_cpu, sd) {
 		int continue_balancing = 1;
 		u64 domain_cost;
+
 
 		if (!(sd->flags & SD_LOAD_BALANCE))
 			continue;
