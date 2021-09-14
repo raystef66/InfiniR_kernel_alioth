104bb4f16f3b93e806e9bbe9721bd73b1a9fb64b
Divyanshu-Modi <divyan.m05@gmail.com>
2021-09-14 10:44:05 +0530

    power: msm-poweroff: silence edl/dl mode dt property missing log
     Silence:
      [    0.096626] Unable to find DT property: qcom,msm-imem-download_mode
      [    0.097830] Unable to find DT property: qcom,msm-imem-emergency_download_mode
    
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -257,7 +257,7 @@ static void *map_prop_mem(const char *propname)
 	void *addr;
 
 	if (!np) {
-		pr_err("Unable to find DT property: %s\n", propname);
+		pr_debug("Unable to find DT property: %s\n", propname);
 		return NULL;
 	}
 
