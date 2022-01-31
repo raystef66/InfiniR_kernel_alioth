480b1fc0ca65730eb24f014fda6575efa275198e
Cyber Knight <cyberknight755@gmail.com>
2022-01-31 15:22:13 +0800

    soc/qcom: msm_bus: msm_bus_fabric_rpmh: Reduce verbosity of logging
    Silences:
    [    0.278039] msm_bus_fab_init_noc_ops: Invalid Bus type
    [    0.278089] msm_bus_fab_init_noc_ops: Invalid Bus type
    [    0.278117] msm_bus_fab_init_noc_ops: Invalid Bus type
    [    0.278163] msm_bus_fab_init_noc_ops: Invalid Bus type
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/soc/qcom/msm_bus/msm_bus_fabric_rpmh.c
+++ drivers/soc/qcom/msm_bus/msm_bus_fabric_rpmh.c
@@ -776,7 +776,7 @@ static void msm_bus_fab_init_noc_ops(struct msm_bus_node_device_type *bus_dev)
 		msm_bus_bimc_set_ops(bus_dev);
 		break;
 	default:
-		MSM_BUS_ERR("%s: Invalid Bus type", __func__);
+		MSM_BUS_DBG("%s: Invalid Bus type", __func__);
 	}
 }
 
