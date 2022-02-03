29e786be81514b3258fb40aed51fcd11a04e4d3b
Cyber Knight <cyberknight755@gmail.com>
2022-02-03 15:42:51 +0800

    msm_geni_serial: Fix logspam with !IPC_LOGGING
    Silences:
    [    0.522752] msm_geni_serial a8c000.qcom,qup_uart: Failed to create dbg dir
    [    0.522759] msm_geni_serial a8c000.qcom,qup_uart: Err in Rx IPC Log
    [    0.522761] msm_geni_serial a8c000.qcom,qup_uart: Err in Tx IPC Log
    [    0.522762] msm_geni_serial a8c000.qcom,qup_uart: Err in Pwr IPC Log
    [    0.522763] msm_geni_serial a8c000.qcom,qup_uart: Err in Misc IPC Log
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/tty/serial/msm_geni_serial.c
+++ drivers/tty/serial/msm_geni_serial.c
@@ -146,10 +146,14 @@
  */
 #define POLL_ITERATIONS		1000
 
+#ifdef CONFIG_IPC_LOGGING
 #define IPC_LOG_MSG(ctx, x...) do { \
 	if (ctx) \
 		ipc_log_string(ctx, x); \
 } while (0)
+#else
+#define IPC_LOG_MSG(x, y...) // no-ops
+#endif
 
 #define DMA_RX_BUF_SIZE		(2048)
 #define UART_CONSOLE_RX_WM	(2)
@@ -3113,6 +3117,7 @@ static void console_unregister(struct uart_driver *drv)
 }
 #endif /* defined(CONFIG_SERIAL_CORE_CONSOLE) || defined(CONFIG_CONSOLE_POLL) */
 
+#if defined(CONFIG_DEBUG_FS) && defined(CONFIG_IPC_LOGGING)
 static void msm_geni_serial_debug_init(struct uart_port *uport, bool console)
 {
 	struct msm_geni_serial_port *msm_port = GET_DEV_PORT(uport);
@@ -3190,6 +3195,11 @@ static void msm_geni_serial_debug_init(struct uart_port *uport, bool console)
 		}
 	}
 }
+#else
+static void msm_geni_serial_debug_init(struct uart_port *uport, bool console)
+{
+}
+#endif
 
 static void msm_geni_serial_cons_pm(struct uart_port *uport,
 		unsigned int new_state, unsigned int old_state)
