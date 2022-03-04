ce2783260abac7f886e454f10d83c43ea96d976b
¨raystef66¨ <s.vanbarel@gmail.com>
2022-03-04 11:57:43 +0100

    InfiniR Alioth v2.07

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 
 KERNEL_DEFCONFIG=vendor/alioth_defconfig
 ANYKERNEL3_DIR=$PWD/AnyKernel3/
-FINAL_KERNEL_ZIP=InfiniR_Alioth_v2.06.zip
+FINAL_KERNEL_ZIP=InfiniR_Alioth_v2.07.zip
 export ARCH=arm64
 
 # Speed up build process
