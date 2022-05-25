2843cd2e1681993276a79966ef39ebed2bab3007
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-05-26 02:53:27 +0600

    devfreq_boost: Skip boosting if kprofiles mode is set to `Battery`
    Mode 1 = Battery. Restrict devfreq boost if kprofiles mode is set
    to 1 aka Battery mode to conserve power.
    
    Adapted from: https://github.com/dakkshesh07/Kprofiles/blob/main/README.md#using-kprofiles-in-your-linux-kernel-source
    
    Co-authored-by: Dakkshesh <dakkshesh5@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -53,9 +53,11 @@ static struct df_boost_drv df_boost_drv_g __read_mostly = {
 		       CONFIG_DEVFREQ_MSM_CPUBW_BOOST_FREQ)
 };
 
+extern int kp_active_mode(void);
+
 static void __devfreq_boost_kick(struct boost_dev *b)
 {
-	if (!READ_ONCE(b->df) || test_bit(SCREEN_OFF, &b->state))
+	if (!READ_ONCE(b->df) || test_bit(SCREEN_OFF, &b->state) || kp_active_mode() == 1)
 		return;
 
 	set_bit(INPUT_BOOST, &b->state);
@@ -77,7 +79,7 @@ static void __devfreq_boost_kick_max(struct boost_dev *b,
 	unsigned long boost_jiffies = msecs_to_jiffies(duration_ms);
 	unsigned long curr_expires, new_expires;
 
-	if (!READ_ONCE(b->df) || test_bit(SCREEN_OFF, &b->state))
+	if (!READ_ONCE(b->df) || test_bit(SCREEN_OFF, &b->state) || kp_active_mode() == 1)
 		return;
 
 	do {
