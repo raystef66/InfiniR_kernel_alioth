263ebf6c9ea37b3e2484c5019edcef083b8a20f2
Cyber Knight <cyberknight755@gmail.com>
2022-06-18 08:53:25 +0800

    drm/atomic: Boost CPU & DDR bus according to set kernel profile
    - Boosting is done only when KProfiles is set to mode 2 or 3.
    - Otherwise boosting is skipped.
    - This patch is adapted to the latest implementation of KProfiles.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -2557,6 +2557,7 @@ static void complete_signaling(struct drm_device *dev,
 	kfree(fence_state);
 }
 
+extern int kp_active_mode(void);
 int drm_mode_atomic_ioctl(struct drm_device *dev,
 			  void *data, struct drm_file *file_priv)
 {
@@ -2598,8 +2599,15 @@ int drm_mode_atomic_ioctl(struct drm_device *dev,
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
 
-	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY))
-		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
+	  /*
+	   * Dont boost CPU & DDR if battery saver profile is enabled
+	   * and boost CPU & DDR for 25ms if balanced profile is enabled
+	   */
+	    devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
+	  } else if (kp_active_mode() == 2) {
+	    devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 25);
+	  }
 
 	drm_modeset_acquire_init(&ctx, DRM_MODESET_ACQUIRE_INTERRUPTIBLE);
 
