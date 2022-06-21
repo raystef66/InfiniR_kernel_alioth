0ebf95ec40b44286448c4767ebcfee070976c38d
Cyber Knight <cyberknight755@gmail.com>
2022-06-21 22:22:07 +0800

    msm_performance: Kill userspace freq manipulation when KProfiles is set to 1
    - We don't need userspace freq manipulation on KProfiles mode 1.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -53,6 +53,7 @@ static unsigned int aggr_big_nr;
 static unsigned int aggr_top_load;
 static unsigned int top_load[CLUSTER_MAX];
 static unsigned int curr_cap[CLUSTER_MAX];
+extern int kp_active_mode(void);
 
 /*******************************sysfs start************************************/
 static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
@@ -63,6 +64,9 @@ static int set_cpu_min_freq(const char *buf, const struct kernel_param *kp)
 	struct cpu_status *i_cpu_stats;
 	struct cpufreq_policy policy;
 	cpumask_var_t limit_mask;
+
+	if (kp_active_mode() == 1)
+	  return 0;
 
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
@@ -142,6 +146,9 @@ static int set_cpu_max_freq(const char *buf, const struct kernel_param *kp)
 	struct cpu_status *i_cpu_stats;
 	struct cpufreq_policy policy;
 	cpumask_var_t limit_mask;
+
+	if (kp_active_mode() == 1)
+	  return 0;
 
 	while ((cp = strpbrk(cp + 1, " :")))
 		ntokens++;
