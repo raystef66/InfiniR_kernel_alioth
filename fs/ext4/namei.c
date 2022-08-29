e555c753265e8f0c0df4279d8b7e5a91b78e6546
¨raystef66¨ <s.vanbarel@gmail.com>
2022-08-29 11:24:31 +0200

    fs/ext4: fix ext4 on v4.19.256 * too few arguments provided to function-like macro invocation

--- fs/ext4/namei.c
+++ fs/ext4/namei.c
@@ -3031,7 +3031,7 @@ bool ext4_empty_dir(struct inode *inode)
 		de = (struct ext4_dir_entry_2 *) (bh->b_data +
 					(offset & (sb->s_blocksize - 1)));
 		if (ext4_check_dir_entry(inode, NULL, de, bh,
-					 bh->b_data, bh->b_size, offset) ||
+					 bh->b_data, bh->b_size, 0, offset) ||
 		    le32_to_cpu(de->inode)) {
 			brelse(bh);
 			return false;
