58773be15bdf6907dc7f012c8d799f7c97509cb5
str143 <alexfinhart@gmail.com>
2022-09-05 18:10:55 +0800

    blkdev: Do not spread I/O tasks
    * imo spreading I/O tasks is quite expensive and recent android 13 changes were about moving binder calls/handlers/observers to async, any/bg threads, we may want to take advantage of these changes.
    
    Signed-off-by: str143 <alexfinhart@gmail.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -725,9 +725,11 @@ struct request_queue {
 #define QUEUE_FLAG_QUIESCED    28	/* queue has been quiesced */
 
 #define QUEUE_FLAG_DEFAULT	((1 << QUEUE_FLAG_SAME_COMP)	|	\
-				 (1 << QUEUE_FLAG_ADD_RANDOM))
+				 (1 << QUEUE_FLAG_NONROT)	|	\
+				 (1 << QUEUE_FLAG_SAME_FORCE))
 
-#define QUEUE_FLAG_MQ_DEFAULT	((1 << QUEUE_FLAG_SAME_COMP)	|	\
+#define QUEUE_FLAG_MQ_DEFAULT	((0 << QUEUE_FLAG_IO_STAT) |		\
+				 (1 << QUEUE_FLAG_SAME_COMP)	|	\
 				 (1 << QUEUE_FLAG_POLL))
 
 void blk_queue_flag_set(unsigned int flag, struct request_queue *q);
