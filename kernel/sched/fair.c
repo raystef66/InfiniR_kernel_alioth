7b5c03964b7f9f8bc23682669329e9df9140256a
str-br <alexfinhart@gmail.com>
2022-10-03 19:57:15 +0800

    sched: fair: Optimize re-scaling of cfs scheduler tunables
    * if the number of online cpus are greater than or equal to 6, use google's scheduler tunings otherwise, use latency-biased tunings
    
    Signed-off-by: str-br <alexfinhart@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -144,7 +144,7 @@ unsigned int sysctl_sched_child_runs_first __read_mostly;
 unsigned int sysctl_sched_wakeup_granularity		= 1000000UL;
 unsigned int normalized_sysctl_sched_wakeup_granularity	= 1000000UL;
 
-const_debug unsigned int sysctl_sched_migration_cost	= 500000UL;
+const_debug unsigned int sysctl_sched_migration_cost	= 750000UL;
 DEFINE_PER_CPU_READ_MOSTLY(int, sched_load_boost);
 
 #ifdef CONFIG_SMP
@@ -236,7 +236,11 @@ static unsigned int get_update_sysctl_factor(void)
 		break;
 	case SCHED_TUNABLESCALING_LOG:
 	default:
-		factor = 1 + ilog2(cpus);
+		if (cpus > 6) {
+		  factor = 2;
+		} else { 
+		  factor = 1;
+		}
 		break;
 	}
 
