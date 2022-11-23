6d871ce8fdf048c15df0328cdcc9b9b905fad0f1
Kazuki Hashimoto <kazukih@tuta.io>
2022-11-23 20:45:21 +0900

    HACK: cpuidle: Don't check if a CPU is isolated
    Those never get applied here.
    
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -685,8 +685,6 @@ static int cpuidle_latency_notify(struct notifier_block *b,
 {
 	unsigned long cpus = atomic_read(&idled) & *cpumask_bits(to_cpumask(v));
 
-	/* Use READ_ONCE to get the isolated mask outside cpu_add_remove_lock */
-	cpus &= ~READ_ONCE(*cpumask_bits(cpu_isolated_mask));
 	if (cpus)
 		arch_send_wakeup_ipi_mask(to_cpumask(&cpus));
 
