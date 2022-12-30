a9bf8c127dea4c0483c396213e215a4a0ccff30f
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2022-12-30 16:42:15 +0600

    subsystem_restart: Do not use WQ_CPU_INTENSIVE in an unbound workqueue allocation
    As per documentation [1], WQ_CPU_INTENSIVE is useful for bound work items which
    are expected to hog CPU cycles so that their execution is regulated by the system
    scheduler and thereby do not contribute to the concurrency level which is pointless
    for an unbound workqueue.
    Therefore, using WQ_CPU_INTENSIVE with WQ_BOUND is redundant. Remove it.
    
    [1]: https://www.kernel.org/doc/html/latest/core-api/workqueue.html#flags
    
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/soc/qcom/subsystem_restart.c
+++ drivers/soc/qcom/subsystem_restart.c
@@ -1971,8 +1971,7 @@ static int __init subsys_restart_init(void)
 {
 	int ret;
 
-	ssr_wq = alloc_workqueue("ssr_wq",
-		WQ_UNBOUND | WQ_HIGHPRI | WQ_CPU_INTENSIVE, 0);
+	ssr_wq = alloc_workqueue("ssr_wq", WQ_UNBOUND | WQ_HIGHPRI, 0);
 	BUG_ON(!ssr_wq);
 
 	ret = bus_register(&subsys_bus_type);
