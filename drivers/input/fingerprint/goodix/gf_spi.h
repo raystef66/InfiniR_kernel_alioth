582eb5128f30445659daa46963d83259b74ec28f
Vaisakh Murali <mvaisakh@statixos.com>
2023-01-12 13:29:23 +0530

    input/fp: goodix: Inline resume, suspend and cleanup functions
    These functions are small and queried extensively, making them inlined
    and static should speed up the function
    
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Signed-off-by: Forenche <prahul2003@gmail.com>
    Signed-off-by: madmax7896 <madmax78968@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/input/fingerprint/goodix/gf_spi.h
+++ drivers/input/fingerprint/goodix/gf_spi.h
@@ -6,6 +6,7 @@
 #ifndef __GF_SPI_H
 #define __GF_SPI_H
 
+#include <linux/gpio.h>
 #include <linux/types.h>
 #include <linux/notifier.h>
 /**********************************************************/
@@ -149,10 +150,63 @@ struct gf_dev {
 };
 
 int gf_parse_dts(struct gf_dev *gf_dev);
-void gf_cleanup(struct gf_dev *gf_dev);
 
-int gf_power_on(struct gf_dev *gf_dev);
-int gf_power_off(struct gf_dev *gf_dev);
+static inline void gf_cleanup(struct gf_dev *gf_dev)
+{
+	pr_info("[info] %s\n", __func__);
+
+	if (gpio_is_valid(gf_dev->irq_gpio)) {
+		gpio_free(gf_dev->irq_gpio);
+		pr_info("remove irq_gpio success\n");
+	}
+
+	if (gpio_is_valid(gf_dev->reset_gpio)) {
+		gpio_free(gf_dev->reset_gpio);
+		pr_info("remove reset_gpio success\n");
+	}
+
+#ifdef GF_PW_CTL
+
+	if (gpio_is_valid(gf_dev->pwr_gpio)) {
+		gpio_free(gf_dev->pwr_gpio);
+		pr_info("remove pwr_gpio success\n");
+	}
+
+#endif
+}
+
+static inline int gf_power_on(struct gf_dev *gf_dev)
+{
+	int rc = 0;
+#ifdef GF_PW_CTL
+
+	if (gpio_is_valid(gf_dev->pwr_gpio)) {
+		rc = gpio_direction_output(gf_dev->pwr_gpio, 1);
+		pr_info("---- power on result: %d----\n", rc);
+	} else {
+		pr_info("%s: gpio_is_invalid\n", __func__);
+	}
+
+#endif
+	msleep(10);
+	return rc;
+}
+
+static inline int gf_power_off(struct gf_dev *gf_dev)
+{
+	int rc = 0;
+#ifdef GF_PW_CTL
+
+	if (gpio_is_valid(gf_dev->pwr_gpio)) {
+		rc = gpio_direction_output(gf_dev->pwr_gpio, 0);
+		pr_info("---- power off result: %d----\n", rc);
+	} else {
+		pr_info("%s: gpio_is_invalid\n", __func__);
+	}
+
+#endif
+	return rc;
+}
 
 int gf_hw_reset(struct gf_dev *gf_dev, unsigned int delay_ms);
 int gf_irq_num(struct gf_dev *gf_dev);
