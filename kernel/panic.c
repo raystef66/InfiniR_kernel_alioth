cded18c3e51cda4ab1ef76d93539e51e36baade9
¨raystef66¨ <s.vanbarel@gmail.com>
2023-02-07 10:22:52 +0100

    external symbol `vendor_panic_cb' can not be used when making a shared object; recompile with -fPIC

--- kernel/panic.c
+++ kernel/panic.c
@@ -241,9 +241,7 @@ void panic(const char *fmt, ...)
 	va_start(args, fmt);
 	vsnprintf(buf, sizeof(buf), fmt, args);
 	va_end(args);
-	dump_stack_minidump(0);
-	if (vendor_panic_cb)
-		vendor_panic_cb(0);
+        dump_stack_minidump(0);
 	pr_emerg("Kernel panic - not syncing: %s\n", buf);
 #ifdef CONFIG_DEBUG_BUGVERBOSE
 	/*
