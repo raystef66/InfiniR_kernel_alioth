9a2dc67cffa855aed2a6eaa9e0cad50a5b069ebc
Pranav-Talmale <pranavtalmale@gmail.com>
2023-04-16 07:03:54 +0530

    techpack: display: modify the existing guards for get_main_display
    - To support exposure_adjustment
    - Driver needs the get_main_display related code to function.

--- techpack/display/msm/dsi/dsi_display.c
+++ techpack/display/msm/dsi/dsi_display.c
@@ -53,7 +53,7 @@ static const struct of_device_id dsi_display_dt_match[] = {
 	{}
 };
 
-#ifdef CONFIG_OSSFOD
+#if defined(CONFIG_OSSFOD) || defined(CONFIG_EXPOSURE_ADJUSTMENT)
 struct dsi_display *primary_display;
 #endif
 static unsigned int cur_refresh_rate = 60;
@@ -6859,7 +6859,7 @@ int dsi_display_get_modes(struct dsi_display *display,
 exit:
 	*out_modes = display->modes;
 	rc = 0;
-#ifdef CONFIG_OSSFOD
+#if defined(CONFIG_OSSFOD) || defined(CONFIG_EXPOSURE_ADJUSTMENT)
 	primary_display = display;
 #endif
 
@@ -8450,7 +8450,7 @@ int dsi_display_unprepare(struct dsi_display *display)
 	return rc;
 }
 
-#ifdef CONFIG_OSSFOD
+#if defined(CONFIG_OSSFOD) || defined(CONFIG_EXPOSURE_ADJUSTMENT)
 struct dsi_display *get_main_display(void)
 {
 	return primary_display;
