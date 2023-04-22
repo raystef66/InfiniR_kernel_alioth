562fe399e44076cff3ff04ca7aba1f995a9c2bdd
NotZeetaa <rodrigo2005contente@gmail.com>
2023-04-22 11:08:57 +0100

    Merge tag 'LA.UM.9.12.r1-15200.02-SMxx50.0' of https://git.codelinaro.org/clo/la/platform/vendor/opensource/display-drivers into staging
    "LA.UM.9.12.r1-15200.02-SMxx50.0"
    
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- techpack/display/msm/dsi/dsi_drm.c
+++ techpack/display/msm/dsi/dsi_drm.c
@@ -196,7 +196,8 @@ static void dsi_bridge_pre_enable(struct drm_bridge *bridge)
 		return;
 	}
 
-	atomic_set(&c_bridge->display->panel->esd_recovery_pending, 0);
+	if (bridge->encoder->crtc->state->active_changed)
+		atomic_set(&c_bridge->display->panel->esd_recovery_pending, 0);
 	mi_cfg = &c_bridge->display->panel->mi_cfg;
 
 	/* By this point mode should have been validated through mode_fixup */
