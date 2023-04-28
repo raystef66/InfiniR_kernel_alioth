f44472087df397af623bf28353524a9cdfe35e93
¨raystef66¨ <s.vanbarel@gmail.com>
2023-04-28 12:52:41 +0200

    Merge tag 'v4.19.282' of https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux into 13.0-alioth
    This is the 4.19.282 stable release

--- net/ipv6/tcp_ipv6.c
+++ net/ipv6/tcp_ipv6.c
@@ -2058,15 +2058,6 @@ static int tcp_v6_init_sock(struct sock *sk)
 	return 0;
 }
 
-#ifndef CONFIG_MPTCP
-static
-#endif
-void tcp_v6_destroy_sock(struct sock *sk)
-{
-	tcp_v4_destroy_sock(sk);
-	inet6_destroy_sock(sk);
-}
-
 #ifdef CONFIG_PROC_FS
 /* Proc filesystem TCPv6 sock list dumping. */
 static void get_openreq6(struct seq_file *seq,
@@ -2263,7 +2254,7 @@ struct proto tcpv6_prot = {
 	.accept			= inet_csk_accept,
 	.ioctl			= tcp_ioctl,
 	.init			= tcp_v6_init_sock,
-	.destroy		= tcp_v6_destroy_sock,
+	.destroy		= tcp_v4_destroy_sock,
 	.shutdown		= tcp_shutdown,
 	.setsockopt		= tcp_setsockopt,
 	.getsockopt		= tcp_getsockopt,
