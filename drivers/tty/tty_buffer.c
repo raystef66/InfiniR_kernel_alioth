9004c67a8707dbe4852fd397abf6d5e0ef0e8bb0
Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
2023-04-30 04:07:16 +0600

    tty: tty_buffer: Use upstream function names with kthread as prefix
    On msm-4.19 and later, queue_kthread_work(), init_kthread_work(), init_kthread_worker(),
    flush_kthread_work() have moved kthread from their names to the beginning and used as
    the prefix. Switch to the upstream version of the function names to avoid compilation
    error.
    
    Fixes: 734e72f ("tty: move tty_port workqueue to be a kthread")
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/tty/tty_buffer.c
+++ drivers/tty/tty_buffer.c
@@ -72,7 +72,7 @@ void tty_buffer_unlock_exclusive(struct tty_port *port)
 	atomic_dec(&buf->priority);
 	mutex_unlock(&buf->lock);
 	if (restart)
-		queue_kthread_work(&port->worker, &buf->work);
+		kthread_queue_work(&port->worker, &buf->work);
 }
 EXPORT_SYMBOL_GPL(tty_buffer_unlock_exclusive);
 
@@ -544,7 +544,7 @@ void tty_flip_buffer_push(struct tty_port *port)
 	struct tty_bufhead *buf = &port->buf;
 
 	tty_flip_buffer_commit(buf->tail);
-	queue_kthread_work(&port->worker, &buf->work);
+	kthread_queue_work(&port->worker, &buf->work);
 }
 EXPORT_SYMBOL(tty_flip_buffer_push);
 
@@ -599,8 +599,8 @@ void tty_buffer_init(struct tty_port *port)
 	atomic_set(&buf->mem_used, 0);
 	atomic_set(&buf->priority, 0);
 	buf->mem_limit = TTYB_DEFAULT_MEM_LIMIT;
-	init_kthread_work(&buf->work, flush_to_ldisc);
-	init_kthread_worker(&port->worker);
+	kthread_init_work(&buf->work, flush_to_ldisc);
+	kthread_init_worker(&port->worker);
 	port->worker_thread = kthread_run(kthread_worker_fn, &port->worker,
 					  "tty_worker_thread");
 	if (IS_ERR(port->worker_thread))
@@ -636,7 +636,7 @@ void tty_buffer_set_lock_subclass(struct tty_port *port)
 
 bool tty_buffer_restart_work(struct tty_port *port)
 {
-	return queue_kthread_work(&port->worker, &port->buf.work);
+	return kthread_queue_work(&port->worker, &port->buf.work);
 }
 
 bool tty_buffer_cancel_work(struct tty_port *port)
@@ -646,5 +646,5 @@ bool tty_buffer_cancel_work(struct tty_port *port)
 
 void tty_buffer_flush_work(struct tty_port *port)
 {
-	flush_kthread_work(&port->buf.work);
+	kthread_flush_work(&port->buf.work);
 }
