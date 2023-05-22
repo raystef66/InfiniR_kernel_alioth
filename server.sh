1200b70333aea6be376040df5fc32bee2af7719c
Rohail33 <rohailyousafzai49@gmail.com>
2023-05-22 22:47:20 +0200

    Add Kernel-SU Support

---
+++ server.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#set -e
+## Copy this script inside the kernel directory
+LINKER="lld"
+DIR=`readlink -f .`
+MAIN=`readlink -f ${DIR}/..`
+KERNEL_DEFCONFIG=alioth_defconfig
+export PATH="$MAIN/clang/bin:$PATH"
+export ARCH=arm64
+export SUBARCH=arm64
+export KBUILD_COMPILER_STRING="$($MAIN/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+
+if ! [ -d "$MAIN/clang" ]; then
+echo "ZYC clang not found! Cloning..."
+if ! git clone -q https://gitlab.com/ZyCromerZ/clang.git --depth=1 --single-branch $MAIN/clang; then
+echo "Cloning failed! Aborting..."
+exit 1
+fi
+fi
+KERNEL_DIR=`pwd`
+ZIMAGE_DIR="$KERNEL_DIR/out/arch/arm64/boot"
+# Speed up build process
+MAKE="./makeparallel"
+BUILD_START=$(date +"%s")
+blue='\033[0;34m'
+cyan='\033[0;36m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+nocol='\033[0m'
+
+# kernel-SU add
+curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
+
+echo "**** Kernel defconfig is set to $KERNEL_DEFCONFIG ****"
+echo -e "$blue***********************************************"
+echo "          BUILDING KERNEL          "
+echo -e "***********************************************$nocol"
+make $KERNEL_DEFCONFIG O=out CC=clang
+make -j$(nproc --all) O=out \
+                      CC=clang \
+                      ARCH=arm64 \
+                      CROSS_COMPILE=aarch64-linux-gnu- \
+                      NM=llvm-nm \
+                      OBJDUMP=llvm-objdump \
+                      STRIP=llvm-strip
+
+TIME="$(date "+%Y%m%d-%H%M%S")"
+mkdir -p tmp
+cp -fp $ZIMAGE_DIR/Image.gz tmp
+cp -fp $ZIMAGE_DIR/dtbo.img tmp
+cp -fp $ZIMAGE_DIR/dtb tmp
+cp -rp ./anykernel/* tmp
+cd tmp
+7za a -mx9 tmp.zip *
+cd ..
+rm *.zip
+cp -fp tmp/tmp.zip RealKing-Alioth-Aosp-$TIME.zip
+rm -rf tmp
+echo $TIME
+
+# Kernel-SU remove
+git checkout drivers/Makefile &>/dev/null
+rm -rf KernelSU
+rm -rf drivers/kernelsu
