7241d998a2b8eba05f7491ff22bea3c3d29786a5
Rohail33 <rohailyousafzai49@gmail.com>
2023-05-22 21:58:40 +0200

    [SQUASH]: Remove High Brightness mode
    Revert "techpack: display: Properly guard OSSFOD"
    
    This reverts commit 8f1849f56b738bb00804b31c07627df6a476510e.
    
    Revert "dsi_display:  Use type_map array index 0 for invalid hbm values"
    
    This reverts commit 6a07ca3a4f08b9e46ac50219ebba8681385d94f5.
    
    Revert "dsi_display: Add support for high brightness mode (HBM)"
    
    This reverts commit 28ed7da236b256fac082c42eab056c36540863c7.

--- techpack/display/msm/dsi/dsi_panel.c
+++ techpack/display/msm/dsi/dsi_panel.c
@@ -5145,9 +5145,6 @@ int dsi_panel_enable(struct dsi_panel *panel)
 		return -EINVAL;
 	}
 
-	if (panel->hbm_mode)
-		dsi_panel_apply_hbm_mode(panel);
-
 	mutex_lock(&panel->panel_lock);
 
 	mi_cfg = &panel->mi_cfg;
@@ -5206,7 +5203,6 @@ int dsi_panel_enable(struct dsi_panel *panel)
 	mutex_unlock(&panel->panel_lock);
 	display_utc_time_marker("DSI_CMD_SET_ON");
 
-
 	return rc;
 }
 
@@ -5220,7 +5216,6 @@ int dsi_panel_post_enable(struct dsi_panel *panel)
 	}
 
 	mutex_lock(&panel->panel_lock);
-
 
 	rc = dsi_panel_tx_cmd_set(panel, DSI_CMD_SET_POST_ON);
 	if (rc) {
@@ -5473,28 +5468,5 @@ int dsi_panel_post_unprepare(struct dsi_panel *panel)
 	}
 error:
 	mutex_unlock(&panel->panel_lock);
-	return rc;
-}
-
-int dsi_panel_apply_hbm_mode(struct dsi_panel *panel)
-{
-	static const enum dsi_cmd_set_type type_map[] = {
-		DSI_CMD_SET_MI_HBM_OFF,
-		DSI_CMD_SET_MI_HBM_ON
-	};
-
-	enum dsi_cmd_set_type type;
-	int rc;
-
-	if (panel->hbm_mode >= 0 &&
-		panel->hbm_mode < ARRAY_SIZE(type_map))
-		type = type_map[panel->hbm_mode];
-	else
-		type = type_map[0];
-
-	mutex_lock(&panel->panel_lock);
-	rc = dsi_panel_tx_cmd_set(panel, type);
-	mutex_unlock(&panel->panel_lock);
-
 	return rc;
 }
