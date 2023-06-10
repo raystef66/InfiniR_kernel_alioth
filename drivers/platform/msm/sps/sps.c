9853db2084235c37ec208ab678fe7def0185c16f
Divyanshu-Modi <divyan.m05@gmail.com>
2023-06-10 20:37:56 +0300

    platform: msm: sps: silence ipc log spam when ipc logging is disabled  Silences:  [    0.383634] sps_register_bam_device : unable to create IPC Logging 0 for bam 0x000000000c184000  [    0.383636] sps_register_bam_device : unable to create IPC Logging 1 for bam 0x000000000c184000  [    0.383642] sps_register_bam_device : unable to create IPC Logging 2 for bam 0x000000000c184000  [    0.383647] sps_register_bam_device : unable to create IPC Logging 3 for bam 0x000000000c184000  [    0.383651] sps_register_bam_device : unable to create IPC Logging 4 for bam 0x000000000c184000  [    0.438542] sps_register_bam_device : unable to create IPC Logging 0 for bam 0x0000000001dc4000  [    0.438545] sps_register_bam_device : unable to create IPC Logging 1 for bam 0x0000000001dc4000  [    0.438551] sps_register_bam_device : unable to create IPC Logging 2 for bam 0x0000000001dc4000  [    0.438556] sps_register_bam_device : unable to create IPC Logging 3 for bam 0x0000000001dc4000  [    0.438561] sps_register_bam_device : unable to create IPC Logging 4 for bam 0x0000000001dc4000

--- drivers/platform/msm/sps/sps.c
+++ drivers/platform/msm/sps/sps.c
@@ -2098,7 +2098,9 @@ int sps_register_bam_device(const struct sps_bam_props *bam_props,
 {
 	struct sps_bam *bam = NULL;
 	void __iomem *virt_addr = NULL;
+#ifdef CONFIG_IPC_LOGGING
 	char bam_name[MAX_MSG_LEN];
+#endif
 	u32 manage;
 	int ok;
 	int result;
