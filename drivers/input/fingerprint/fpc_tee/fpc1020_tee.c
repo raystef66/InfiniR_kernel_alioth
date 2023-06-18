724400464c2b778c971e62f79998c58af6bdd284
Kazuki Hashimoto <kazukih@tuta.io>
2023-06-18 12:16:59 +0000

    fpc1020: Micro-optimize interrupt path
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- drivers/input/fingerprint/fpc_tee/fpc1020_tee.c
+++ drivers/input/fingerprint/fpc_tee/fpc1020_tee.c
@@ -925,10 +925,7 @@ static inline irqreturn_t fpc1020_irq_handler(int irq, void *handle)
 
 	dev_dbg(fpc1020->dev, "%s\n", __func__);
 
-	if (atomic_read(&fpc1020->wakeup_enabled)) {
-		fpc1020->nbr_irqs_received++;
-		__pm_wakeup_event(fpc1020->ttw_wl, FPC_TTW_HOLD_TIME);
-	}
+	__pm_wakeup_event(fpc1020->ttw_wl, FPC_TTW_HOLD_TIME);
 
 	sysfs_notify(&fpc1020->dev->kobj, NULL, dev_attr_irq.attr.name);
 #if IS_ENABLED(CONFIG_MI_DRM_OPT)
