f4a223a9b5930831c30b8fa9ef170a6a91f6515b
Rohail33 <rohailyousafzai49@gmail.com>
2023-07-17 16:07:06 +0200

    [SQUASH]: power: supply: Revert All picked commits
    Revert "power_supply: don't allocate attrname"
    
    This reverts commit c4fcd48f67833595ba531300d3335a442301cf11.
    
    Revert "power: supply: Fix logic checking if system is running from battery"
    
    This reverts commit 8f974cfd075570771c8daf33a6480a49d625fc93.
    
    Revert "power: supply: Ratelimit no data debug output"
    
    This reverts commit 89ec3e23ddce8ba76ab6979dc9d1cb1edf493a65.
    
    Revert "power: supply: bq27xxx: Use mod_delayed_work() instead of cancel() + schedule()"
    
    This reverts commit 1086c0ab04c6aab240879698563e6bc7e7e32899.
    
    Revert "power: supply: ab8500: Fix external_power_changed race"
    
    This reverts commit 90aa063ef36f8c163a8c0de77cc82e92a0d12519.
    
    Revert "power: supply: bq24190: Call power_supply_changed() after updating input current"
    
    This reverts commit 1604e1288fb33c90217dd38f853d12500c8977d0.
    
    Revert "power: supply: core: Refactor power_supply_set_input_current_limit_from_supplier()"
    
    This reverts commit d55c6891981d5d0f83f3f0a820fa530e57cca25b.
    
    Revert "power: supply: bq27xxx: After charger plug in/out wait 0.5s for things to stabilize"
    
    This reverts commit 792a73d28ca636f2ff908bb931a02e79e428fb21.
    
    Revert "power: supply: generic-adc-battery: fix unit scaling"
    
    This reverts commit 2245acbd2bed1cacd02d25afae5eeb9f8fda14bb.
    
    Revert "power: smb5: Rerun APSD to ensure proper charger detection"
    
    This reverts commit 0680eca6bf3168695bacea0ed6c674ac774334f6.
    
    Revert "power: smb-lib: Force suspend when cable out."
    
    This reverts commit e59bda3b0891ab5fe37eedb6d14694b2a9a80e76.
    
    Revert "power: smb5: Properly opt-out from s/w based health reporting"
    
    This reverts commit e93892194b50a9c9688d9f0105ab177c7e17f1f0.
    
    Revert "power: fg-alg: support maximum limit for capacity learning"
    
    This reverts commit 04ee0e61d7cc8ac7239bc1093dcb600d1d3774f1.
    
    Revert "power: fg-alg: fix wrong logic in maximum limit"
    
    This reverts commit b361da0e8a1b8a32cd4b5b33ce91732d8abe6313.
    
    Revert "qpnp-qg: fix the 1% resolution limit of charge_counter"
    
    This reverts commit bfaeb305062979e22407e4dc57f35b0a8b3d4d49.
    
    Revert "qpnp-qg: fix incorrect vbatt parameter for good ocv re-trigger"
    
    This reverts commit 83a0820a8337521b768a4edd375a7d2a423c7f98.
    
    Revert "qcom: qpnp-pq: Fix -Wpointer-to-int-cast"
    
    This reverts commit cdc92b65e8e76f1d63a95a1074e800d9fb8da747.
    
    Revert "qpnp-qg: adjust QG_SOC when CC_SOC exceeds SOC_FULL"
    
    This reverts commit d4f98f1811383ec9ab602d384b42781eb5eee352.
    
    Revert "qpnp-qg: fix battery coulomb count not monotonic"
    
    This reverts commit 76e093db6f7af9ba188b6d37890433cd2e663993.
    
    Revert "power: supply: Classify Battery Monitor Systems as batteries"
    
    This reverts commit 9b290e45322b5590ecc69d046c7443775cd79253.
    
    Revert "power: qpnp-fg-gen3: increase battery temp reading precision"
    
    This reverts commit 88a14dfbe77d5cdf222ce3c3ee95ed22a132016a.
    
    Revert "drivers: power: supply: qcom: relax wakeup events and optimize"
    
    This reverts commit d2841d47b1b6ac924b1c9ee2a6eaf3258f094791.

--- drivers/power/supply/qcom/qpnp-fg-gen4.c
+++ drivers/power/supply/qcom/qpnp-fg-gen4.c
@@ -2790,7 +2790,7 @@ done:
 	batt_psy_initialized(fg);
 	fg_notify_charger(fg);
 	power_supply_changed(fg->fg_psy);
-	mod_delayed_work(system_freezable_power_efficient_wq, &chip->ttf->ttf_work, msecs_to_jiffies(10000));
+	schedule_delayed_work(&chip->ttf->ttf_work, msecs_to_jiffies(10000));
 	fg_dbg(fg, FG_STATUS, "profile loaded successfully");
 out:
 	if (!chip->esr_fast_calib || is_debug_batt_id(fg)) {
@@ -2803,10 +2803,10 @@ out:
 		chip->batt_age_level = chip->last_batt_age_level;
 	fg->soc_reporting_ready = true;
 	vote(fg->awake_votable, ESR_FCC_VOTER, true, 0);
-	mod_delayed_work(system_freezable_power_efficient_wq, &chip->pl_enable_work, msecs_to_jiffies(5000));
+	schedule_delayed_work(&chip->pl_enable_work, msecs_to_jiffies(5000));
 	vote(fg->awake_votable, PROFILE_LOAD, false, 0);
 	if (!work_pending(&fg->status_change_work)) {
-		pm_wakeup_event(fg->dev, 0);
+		pm_stay_awake(fg->dev);
 		schedule_work(&fg->status_change_work);
 	}
 
@@ -3845,7 +3845,7 @@ static irqreturn_t fg_batt_missing_irq_handler(int irq, void *data)
 	}
 
 	clear_battery_profile(fg);
-	mod_delayed_work(system_freezable_power_efficient_wq, &fg->profile_load_work, 0);
+	schedule_delayed_work(&fg->profile_load_work, 0);
 
 	if (fg->fg_psy)
 		power_supply_changed(fg->fg_psy);
@@ -4160,7 +4160,7 @@ static enum alarmtimer_restart fg_esr_fast_cal_timer(struct alarm *alarm,
 		 * We cannot vote for awake votable here as that takes
 		 * a mutex lock and this is executed in an atomic context.
 		 */
-		pm_wakeup_event(fg->dev, 0);
+		pm_stay_awake(fg->dev);
 		chip->esr_fast_cal_timer_expired = true;
 		schedule_work(&chip->esr_calib_work);
 	}
@@ -4813,7 +4813,7 @@ static void sram_dump_work(struct work_struct *work)
 	fg_dbg(fg, FG_STATUS, "SRAM Dump done at %lld.%d\n",
 		quotient, remainder);
 resched:
-	mod_delayed_work(system_freezable_power_efficient_wq, &fg->sram_dump_work,
+	schedule_delayed_work(&fg->sram_dump_work,
 			msecs_to_jiffies(fg_sram_dump_period_ms));
 }
 
@@ -4852,7 +4852,7 @@ static ssize_t sram_dump_en_store(struct device *dev, struct device_attribute
 	}
 
 	if (fg_sram_dump)
-		mod_delayed_work(system_freezable_power_efficient_wq, &fg->sram_dump_work,
+		schedule_delayed_work(&fg->sram_dump_work,
 				msecs_to_jiffies(fg_sram_dump_period_ms));
 	else
 		cancel_delayed_work_sync(&fg->sram_dump_work);
@@ -5474,7 +5474,7 @@ static int fg_psy_set_property(struct power_supply *psy,
 			return -EINVAL;
 		chip->last_batt_age_level = chip->batt_age_level;
 		chip->batt_age_level = pval->intval;
-		mod_delayed_work(system_freezable_power_efficient_wq, &fg->profile_load_work, 0);
+		schedule_delayed_work(&fg->profile_load_work, 0);
 		break;
 	case POWER_SUPPLY_PROP_CALIBRATE:
 		rc = fg_gen4_set_calibrate_level(chip, pval->intval);
@@ -5629,7 +5629,7 @@ static int fg_notifier_cb(struct notifier_block *nb,
 		 * We cannot vote for awake votable here as that takes
 		 * a mutex lock and this is executed in an atomic context.
 		 */
-		pm_wakeup_event(fg->dev, 0);
+		pm_stay_awake(fg->dev);
 		schedule_work(&fg->status_change_work);
 	}
 
@@ -5642,7 +5642,7 @@ static int fg_awake_cb(struct votable *votable, void *data, int awake,
 	struct fg_dev *fg = data;
 
 	if (awake)
-		pm_wakeup_event(fg->dev, 0);
+		pm_stay_awake(fg->dev);
 	else
 		pm_relax(fg->dev);
 
@@ -7204,7 +7204,7 @@ static void empty_restart_fg_work(struct work_struct *work)
 			schedule_delayed_work(&fg->soc_monitor_work,
 				msecs_to_jiffies(RESTART_FG_MONITOR_SOC_WAIT_PER_MS));
 		} else {
-			mod_delayed_work(system_freezable_power_efficient_wq, 
+			schedule_delayed_work(
 					&fg->empty_restart_fg_work,
 					msecs_to_jiffies(RESTART_FG_WORK_MS));
 		}
@@ -7413,9 +7413,9 @@ static int fg_gen4_probe(struct platform_device *pdev)
 	INIT_WORK(&fg->status_change_work, status_change_work);
 	INIT_WORK(&chip->esr_calib_work, esr_calib_work);
 	INIT_WORK(&chip->soc_scale_work, soc_scale_work);
-	INIT_DEFERRABLE_WORK(&fg->profile_load_work, profile_load_work);
-	INIT_DEFERRABLE_WORK(&fg->sram_dump_work, sram_dump_work);
-	INIT_DEFERRABLE_WORK(&chip->pl_enable_work, pl_enable_work);
+	INIT_DELAYED_WORK(&fg->profile_load_work, profile_load_work);
+	INIT_DELAYED_WORK(&fg->sram_dump_work, sram_dump_work);
+	INIT_DELAYED_WORK(&chip->pl_enable_work, pl_enable_work);
 	INIT_WORK(&chip->pl_current_en_work, pl_current_en_work);
 	INIT_DELAYED_WORK(&fg->soc_monitor_work, soc_monitor_work);
 	INIT_DELAYED_WORK(&fg->empty_restart_fg_work, empty_restart_fg_work);
@@ -7608,13 +7608,15 @@ static int fg_gen4_probe(struct platform_device *pdev)
 
 	device_init_wakeup(fg->dev, true);
 	if (!fg->battery_missing)
-		mod_delayed_work(system_freezable_power_efficient_wq, &fg->profile_load_work, 0);
+		schedule_delayed_work(&fg->profile_load_work, 0);
 
 	fg_gen4_post_init(chip);
 	if (chip->dt.fg_increase_100soc_time) {
-        mod_delayed_work(system_freezable_power_efficient_wq, &fg->soc_monitor_work, msecs_to_jiffies(0));
+		schedule_delayed_work(&fg->soc_monitor_work,
+			msecs_to_jiffies(0));
 	} else {
-        mod_delayed_work(system_freezable_power_efficient_wq, &fg->soc_monitor_work, msecs_to_jiffies(5*MONITOR_SOC_WAIT_MS));
+		schedule_delayed_work(&fg->soc_monitor_work,
+			msecs_to_jiffies(5*MONITOR_SOC_WAIT_MS));
 	}
 
 	/*
@@ -7625,7 +7627,7 @@ static int fg_gen4_probe(struct platform_device *pdev)
 	 */
 	if ((volt_uv >= VBAT_RESTART_FG_EMPTY_UV)
 			&& (msoc == 0) && (batt_temp >= TEMP_THR_RESTART_FG))
-		mod_delayed_work(system_freezable_power_efficient_wq, &fg->empty_restart_fg_work,
+		schedule_delayed_work(&fg->empty_restart_fg_work,
 				msecs_to_jiffies(RESTART_FG_START_WORK_MS));
 	pr_debug("FG GEN4 driver probed successfully\n");
 	return 0;
@@ -7708,9 +7710,9 @@ static int fg_gen4_resume(struct device *dev)
 	struct fg_gen4_chip *chip = dev_get_drvdata(dev);
 	struct fg_dev *fg = &chip->fg;
 
-	mod_delayed_work(system_freezable_power_efficient_wq, &chip->ttf->ttf_work, 0);
+	schedule_delayed_work(&chip->ttf->ttf_work, 0);
 	if (fg_sram_dump)
-		mod_delayed_work(system_freezable_power_efficient_wq, &fg->sram_dump_work,
+		schedule_delayed_work(&fg->sram_dump_work,
 				msecs_to_jiffies(fg_sram_dump_period_ms));
 
 	schedule_delayed_work(&fg->soc_monitor_work,
