daf7aa612c2f48d3aa06a562cbd62c75b3d6a16a
¨raystef66¨ <s.vanbarel@gmail.com>
2023-07-22 12:53:24 +0200

    InfiniR Alioth v2.69 * update to latest kSU_11151 (v0.6.2 ready) * address fast charging for some users * address random reboot for some users

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 
 KERNEL_DEFCONFIG=alioth_defconfig
 ANYKERNEL3_DIR=$PWD/AnyKernel3/
-FINAL_KERNEL_ZIP=InfiniR_Alioth_v2.68.zip
+FINAL_KERNEL_ZIP=InfiniR_Alioth_v2.69.zip
 export ARCH=arm64
 
 # Speed up build process
