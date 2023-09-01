71323fc34ac02c699eb010a5fc4fb01e065cc8ed
¨raystef66¨ <s.vanbarel@gmail.com>
2023-09-01 14:39:05 +0200

    InfiniR Alioth v2.73 * update to latest kSU (v0.6.7 ready) * merge latest v4.19.293

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 
 KERNEL_DEFCONFIG=alioth_defconfig
 ANYKERNEL3_DIR=$PWD/AnyKernel3/
-FINAL_KERNEL_ZIP=InfiniR_Alioth_v2.72.zip
+FINAL_KERNEL_ZIP=InfiniR_Alioth_v2.73.zip
 export ARCH=arm64
 
 # Speed up build process
