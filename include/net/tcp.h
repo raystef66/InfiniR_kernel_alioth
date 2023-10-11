9afe98b9a207d7cced76758d250855b4f97fbd6d
Danda420 <dandagaharu77@gmail.com>
2023-10-11 07:30:45 +0700

    Merge tag 'v4.19.296' of https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux
    This is the 4.19.296 stable release

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -485,12 +485,14 @@ extern int tcp_proc_delayed_ack_control(struct ctl_table *table, int write,
 				loff_t *ppos);
 
 void tcp_enter_quickack_mode(struct sock *sk, unsigned int max_quickacks);
-static inline void tcp_dec_quickack_mode(struct sock *sk,
-					 const unsigned int pkts)
+static inline void tcp_dec_quickack_mode(struct sock *sk)
 {
 	struct inet_connection_sock *icsk = inet_csk(sk);
 
 	if (icsk->icsk_ack.quick) {
+		/* How many ACKs S/ACKing new data have we sent? */
+		const unsigned int pkts = inet_csk_ack_scheduled(sk) ? 1 : 0;
+
 		if (pkts >= icsk->icsk_ack.quick) {
 			icsk->icsk_ack.quick = 0;
 			/* Leaving quickack mode we deflate ATO. */
