26e44cb77940af8f0d8f99c6b5fc15094852c964
¨raystef66¨ <s.vanbarel@gmail.com>
2023-10-13 09:04:25 +0200

    drivers/base/arch_topology.c: fix build error ../drivers/base/arch_topology.c:115:6: error: implicit declaration of function 'is_sched_lib_based_app' [-Werror,-Wimplicit-function-declaration]         if (is_sched_lib_based_app(current->pid))             ^ ../drivers/base/arch_topology.c:118:65: error: too few arguments to function call, expected 2, have 1         return sprintf(buf, "%lu\n", topology_get_cpu_scale(cpu->dev.id));

--- drivers/base/arch_topology.c
+++ drivers/base/arch_topology.c
@@ -112,10 +112,7 @@ static ssize_t cpu_capacity_show(struct device *dev,
 {
 	struct cpu *cpu = container_of(dev, struct cpu, dev);
 
-	if (is_sched_lib_based_app(current->pid))
-		return scnprintf(buf, PAGE_SIZE, "%lu\n", SCHED_CAPACITY_SCALE);
-
-	return sprintf(buf, "%lu\n", topology_get_cpu_scale(cpu->dev.id));
+	return sysfs_emit(buf, "%lu\n", topology_get_cpu_scale(NULL, cpu->dev.id));
 }
 
 static void update_topology_flags_workfn(struct work_struct *work);
