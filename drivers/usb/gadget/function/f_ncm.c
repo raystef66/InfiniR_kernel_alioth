88b395c4fc62fb5ff7b7ac75f9fd8eccfabd9fa1
Danda420 <dandagaharu77@gmail.com>
2023-11-30 22:00:43 +0700

    Merge tag 'v4.19.300' of https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux
    This is the 4.19.300 stable release

--- drivers/usb/gadget/function/f_ncm.c
+++ drivers/usb/gadget/function/f_ncm.c
@@ -1435,7 +1435,7 @@ static int ncm_bind(struct usb_configuration *c, struct usb_function *f)
 	struct usb_composite_dev *cdev = c->cdev;
 	struct f_ncm		*ncm = func_to_ncm(f);
 	struct usb_string	*us;
-	int			status;
+	int			status = 0;
 	struct usb_ep		*ep;
 	struct f_ncm_opts	*ncm_opts;
 
@@ -1453,41 +1453,16 @@ static int ncm_bind(struct usb_configuration *c, struct usb_function *f)
 		f->os_desc_table[0].os_desc = &ncm_opts->ncm_os_desc;
 	}
 
-	/*
-	 * in drivers/usb/gadget/configfs.c:configfs_composite_bind()
-	 * configurations are bound in sequence with list_for_each_entry,
-	 * in each configuration its functions are bound in sequence
-	 * with list_for_each_entry, so we assume no race condition
-	 * with regard to ncm_opts->bound access
-	 */
-	if (!ncm_opts->bound) {
-		mutex_lock(&ncm_opts->lock);
-		ncm_opts->net = gether_setup_default();
-		if (IS_ERR(ncm_opts->net)) {
-			status = PTR_ERR(ncm_opts->net);
-			mutex_unlock(&ncm_opts->lock);
-			goto error;
-		}
-		gether_set_gadget(ncm_opts->net, cdev->gadget);
+	mutex_lock(&ncm_opts->lock);
+	gether_set_gadget(ncm_opts->net, cdev->gadget);
+	if (!ncm_opts->bound)
 		status = gether_register_netdev(ncm_opts->net);
-		mutex_unlock(&ncm_opts->lock);
-		if (status) {
-			free_netdev(ncm_opts->net);
-			goto fail;
-		}
-		ncm_opts->bound = true;
-	}
+	mutex_unlock(&ncm_opts->lock);
 
-	/* export host's Ethernet address in CDC format */
-	status = gether_get_host_addr_cdc(ncm_opts->net, ncm->ethaddr,
-				      sizeof(ncm->ethaddr));
-	if (status < 12) { /* strlen("01234567890a") */
-		ERROR(cdev, "%s: failed to get host eth addr, err %d\n",
-		__func__, status);
-		status = -EINVAL;
-		goto netdev_cleanup;
-	}
-	ncm->port.ioport = netdev_priv(ncm_opts->net);
+	if (status)
+		goto fail;
+
+	ncm_opts->bound = true;
 
 	us = usb_gstrings_attach(cdev, ncm_strings,
 				 ARRAY_SIZE(ncm_string_defs));
@@ -1601,12 +1576,11 @@ fail:
 		kfree(ncm->notify_req->buf);
 		usb_ep_free_request(ncm->notify, ncm->notify_req);
 	}
-netdev_cleanup:
+/*netdev_cleanup:
 	gether_cleanup(netdev_priv(ncm_opts->net));
-
 error:
 	ERROR(cdev, "%s: can't bind, err %d\n", f->name, status);
-
+*/
 	return status;
 }
 
