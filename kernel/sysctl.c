643ecb7772b2ddc2c4bb587e5a25801a7c90746f
Rohail33 <rohailyousafzai49@gmail.com>
2024-03-02 20:53:50 +0000

    Merge tag 'v4.19.308' of https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux
    This is the 4.19.308 stable release
    
     Conflicts:
    	include/linux/fs.h

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -131,6 +131,7 @@ static int __maybe_unused one = 1;
 static int __maybe_unused two = 2;
 static int __maybe_unused three = 3;
 static int __maybe_unused four = 4;
+static int int_max = INT_MAX;
 static unsigned long zero_ul;
 static unsigned long long_max = LONG_MAX;
 static int one_hundred = 100;
@@ -779,6 +780,8 @@ static struct ctl_table kern_table[] = {
 		.maxlen		= sizeof(unsigned int),
 		.mode		= 0644,
 		.proc_handler	= sched_rt_handler,
+		.extra1		= &one,
+		.extra2		= &int_max,
 	},
 	{
 		.procname	= "sched_rt_runtime_us",
@@ -786,6 +789,8 @@ static struct ctl_table kern_table[] = {
 		.maxlen		= sizeof(int),
 		.mode		= 0644,
 		.proc_handler	= sched_rt_handler,
+		.extra1		= &neg_one,
+		.extra2		= &int_max,
 	},
 	{
 		.procname	= "sched_rr_timeslice_ms",
